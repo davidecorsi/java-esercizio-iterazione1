46f4fdad3e487245c305c02be763e80a7c6a8ee0
Davide Corsi <davide.corsi@par-tec.it>
2021-10-01 10:27:23 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+/*
+ * Scrivete un applicazione che conti una serie di numeri finchè il totale dei numeri non superi 1000.
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	}
+}
